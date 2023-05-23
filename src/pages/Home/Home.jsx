478b563829d16c45fcc91daabaf9cb223b26820b
Ajarek <ajarek@poczta.onet.pl>
2023-05-23 23:06:17 +0200

    initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+    <h1>Apartamenty w Kołobrzegu</h1>
+   <p>Oferujemy najwyższej klasy apartamenty , położone w sąsiedztwie plaż oraz centrów komunikacyjnych miasta Kołobrzeg, zadowolą zarówno tych poszukujących ciszy i wypoczynku, jak i entuzjastów wieczornych szaleństw.</p>
+   <button>Wybierz swój Apartament</button>
+  </div>
+  )
+}
+
+export default Home
