f976cb44cd40ea5ce210c5c7950fd5d03c4319bd
Ajarek <ajarek@poczta.onet.pl>
2023-05-26 22:19:02 +0200

    added Contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,13 +1,75 @@
+import {
+  FaGithub,
+  FaTwitter,
+  FaLinkedinIn,
+  FaFacebook,
+  FaPhoneAlt,
+  FaMailBulk,
+  FaMapMarkerAlt,
+} from 'react-icons/fa'
+import { Form } from '../../components/Form/Form'
 import './Contact.css'
-
-import './Contact.css'
-
 export const Contact = () => {
-
-  
   return (
     <div className='contact'>
-     Contact
+      <div className='left-wrapper'>
+        <h1>Kontakt</h1>
+        <a
+          className={'link'}
+          href='tel:+48573219230'
+        >
+          <FaPhoneAlt /> +48 573 219 230
+        </a>
+        <a
+          className={'link'}
+          href='mailto:ajarek@poczta.onet.pl'
+        >
+          <FaMailBulk /> ajarek@poczta.onet.pl
+        </a>
+        <a href='#'>
+          <FaMapMarkerAlt /> Poland Kołobrzeg{' '}
+        </a>
+      </div>
+      <div className='right-wrapper'>
+        <p>Napisz do Nas 🖊️ :</p>
+        <Form />
+      </div>
+      <div className='footer'>
+        <a
+          className={'link'}
+          href='mailto:ajarek@poczta.onet.pl'
+        >
+          <div className='logo'>
+            <img
+              src='my-logo.png'
+              alt='logo'
+            />
+            @jarek
+          </div>
+        </a>
+        <div className='socials'>
+          <div className='social'>
+            <a href='#'>
+              <FaGithub />
+            </a>
+          </div>
+          <div className='social'>
+            <a href='#'>
+              <FaFacebook />
+            </a>
+          </div>
+          <div className='social'>
+            <a href='#'>
+              <FaLinkedinIn />
+            </a>
+          </div>
+          <div className='social'>
+            <a href='#'>
+              <FaTwitter />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
